4d3fd93c05ebf72b12aca445455d13bf6f8b013c
SKDK <muthukumaran.shanmugam@directlinegroup.co.uk>
2021-04-21 17:38:47 +0100

    Initial commit

---
+++ src/test/java/com/learnpkg/test/getChromeDriver.java
@@ -0,0 +1,13 @@
+package com.learnpkg.test;
+
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class getChromeDriver
+{
+
+    public ChromeDriver getDriver(){
+        ChromeDriver driver = new ChromeDriver();
+        return driver;
+    }
+}
